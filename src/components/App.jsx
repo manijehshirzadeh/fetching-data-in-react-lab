3190b5a2814b126773f9f858b4e9a72c6b351011
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-08 18:00:01 +1000

    Add fetching data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,25 @@
+// import StarshipSearch from "./StarshipSearch";
+import StarshipList from "./StarshipList";
+import { getAllStartships } from "../services/starshipService";
+
+import { useState, useEffect } from "react";
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [starships, setStarships] = useState([]);
+
+  const fetchData = async () => {
+    const data = await getAllStartships();
+
+    setStarships(data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>{starships.length !== 0 && <StarshipList starships={starships} />}</>
+  );
 };
 
 export default App;
---
+++ src/components/StarshipList.jsx
@@ -0,0 +1,7 @@
+// import StarshipCard from "./StarshipCard";
+const StarshipList = (props) => {
+  console.log(props.starships);
+  return <>Hi </>;
+};
+
+export default StarshipList;
